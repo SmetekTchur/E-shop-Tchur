4759e55bc22be4d88d6c285cf15a236a9b852fcb
Zmar <159122092+SmetekTchur@users.noreply.github.com>
2025-02-16 22:33:02 +0100

    HTML,CSS,JS_done
    Doděláno Produkty, byl navrácen Afroamerický muž s oblekem a občasně držící dětský olejíček (jenom reklama)

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-
+function zmenProdukt(cisloProduktu) {
+    
+    document.querySelectorAll('.product-info').forEach(produkt => {
+        produkt.classList.remove('active');
+    });
+    
+    
+    document.getElementById(`produkt-${cisloProduktu}`).classList.add('active');
+    
+    
+    document.querySelectorAll('.thumbnail').forEach((obrazek, index) => {
+        obrazek.classList.toggle('active', index === cisloProduktu);
+    });
+}
